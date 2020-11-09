df625f3fce7aa434d2262dcc1fdf89b89bc129f0
Edgar <mmnledgar@gmail.com>
2020-11-09 13:07:36 +0000

    finish_all_views

--- src/main/java/com/example/securityApp/controllers/ViewController.java
+++ src/main/java/com/example/securityApp/controllers/ViewController.java
@@ -1,34 +1,40 @@
 package com.example.securityApp.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.example.securityApp.dao.UserRepository;
+import com.example.securityApp.dao.VillageRepository;
 
 @Controller
 public class ViewController {
 	
 	private ModelAndView mdv;
+	@Autowired
+	private VillageRepository iVillage;
 	
 	public ViewController() {
 		mdv = new ModelAndView();
 	}
 	
-//	@GetMapping(value= {"/"})
-//	public ModelAndView getHomePage() {
-//		this.mdv.addObject("nom", "SECK NGOR");
-//		this.mdv.setViewName("/home/Home");
-//		return this.mdv;
-//	}
-	
-//	@GetMapping(value= {"/"})
-//	public ModelAndView getHomePage() {
-//		mdv.addObject("view", "CLIENT | PAGE");
-//		mdv.setViewName("/home/client");
-//		return this.mdv;
-//	}
+	//@GetMapping(value= {"/"})
+	public ModelAndView getHomePage(){
+		this.mdv.addObject("nom", "SECK NGOR");
+		this.mdv.setViewName("/home/Home");
+		return this.mdv;
+	}
 	
 	@GetMapping(value= {"/"})
-	public ModelAndView getHomePage() {
+	public ModelAndView getClientPage() {
+		this.mdv.addObject("villages", iVillage.findAll());
+		mdv.setViewName("/home/client");
+		return this.mdv;
+	}
+	
+	@GetMapping(value= {"/village"})
+	public ModelAndView getVillagePage() {
 		mdv.setViewName("/home/village");
 		return this.mdv;
 	}
