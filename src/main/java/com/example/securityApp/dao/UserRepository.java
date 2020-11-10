765516848867f411aa83074dfe38e0dec6f9942e
Edgar <mmnledgar@gmail.com>
2020-11-10 14:39:58 +0000

    finish_security

--- src/main/java/com/example/securityApp/dao/UserRepository.java
+++ src/main/java/com/example/securityApp/dao/UserRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.securityApp.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
-	public User findByEmail(String email);
+	public User findByEmailAndPasword(String email,String password);
 }
--- src/main/java/com/example/securityApp/security/UserDet.java
+++ src/main/java/com/example/securityApp/security/UserDet.java
@@ -1,11 +1,15 @@
 package com.example.securityApp.security;
 
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 import com.example.securityApp.dao.UserRepository;
 import com.example.securityApp.entities.User;
@@ -18,8 +22,14 @@ public class UserDet implements UserDetailsService {
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		User u =userrepo.findByEmail(username);
+		//get the email and the password
+		HttpServletRequest req = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+		String password = req.getParameter("password");
 		
+		//fetch from the database 
+		User u =userrepo.findByEmailAndPasword(username,password);
+		
+		//throw the error exception
 		if(u==null)throw new UsernameNotFoundException("user not found");
 		
 		return new UserPrinciple(u);
